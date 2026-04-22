322423db128011ec7a174fa117948fded5519ada
Muhammad Uzair <uzair.nasir@persistentsolutions.co>
2026-04-22 11:39:45 +0500

    raza new chngs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
@@ -36,6 +36,7 @@ import { ProfileComponent } from './Login-Register/profile/profile.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     // FormsModule,
     FormsModule,
     HttpClientModule
